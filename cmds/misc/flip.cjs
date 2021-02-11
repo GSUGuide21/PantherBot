550bf37ec472b8830336d9a6121de1dd79a99f1a
breoliver205 <58759617+breoliver205@users.noreply.github.com>
2021-02-11 13:54:07 -0500

    Create flip.cjs

---
+++ cmds/misc/flip.cjs
@@ -0,0 +1,19 @@
+import { Command } from "discord.js-commando";
+
+export default class FlipCommand extends Command { 
+	constructor( bot ) { 
+		super( bot, { 
+			name : "flip",
+			memberName : "flip",
+			group : "misc",
+			description : "Allows PantherBot to perform a coin flip"
+		} );
+	}
+
+	async run( message ) { 
+		const coins = Object.freeze( [ "Heads", "Tails" ] );
+		const randomIndex = Math.floor( Math.random( ) * coins.length );
+		const coin = coins[ randomIndex ];
+		return message.channel.send( `PantherBot has flipped a coin. It was ${ coin }!` );
+	}
+}
