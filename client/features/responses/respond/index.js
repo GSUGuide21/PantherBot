d02e82d1ff6958be5135236c80bf3dc9399452a5
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2021-06-15 02:19:47 -0400

    Update index.js

--- client/features/responses/respond/index.js
+++ client/features/responses/respond/index.js
@@ -15,5 +15,14 @@ module.exports = {
 
 		const number = Math.floor( ( Math.random( ) * 6 ) + 1 );
 		return channel.send( `PantherBot has rolled the dice. The number is ${number}!` );
+	},
+	flip: async message => { 
+		const { channel } = message;
+
+		const coins = Object.freeze( [ "Heads", "Tails" ] );
+		const index = Math.floor( Math.random( ) * coins.length );
+		const coin = coins[ index ];
+
+		return channel.send( `PantherBot has flipped a coin. You got ${coin}!` );
 	}
 };
