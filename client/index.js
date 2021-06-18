209cc97231e9c8b710d9d443e892192356e1bfa4
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2021-06-18 00:59:29 -0400

    Update index.js

--- client/index.js
+++ client/index.js
@@ -702,7 +702,9 @@ module.exports = class PantherBotClient extends Client {
 	async connect( ) { 
 		return mongoose.connect( process.env.MONGO_URI, { 
 			useNewUrlParser: true,
-			useUnifiedTopology: true
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false
 		} );
 	}
 
