12bc1759547ac7742e951e0820e72ca3b1160d41
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2021-08-12 13:53:32 -0400

    Update signal.js

--- client/features/commands/Main/signal.js
+++ client/features/commands/Main/signal.js
@@ -77,7 +77,7 @@ module.exports = class SignalCommand extends PantherBotCommand {
 	 * @returns 
 	 */
 	run = async ( { channel }, args ) => { 
-		const title = await args.pick( "string" );
+		const title = await args.restResult( "string" );
 		if ( !title ) channel.send( { content: "Failed to fetch the Signal article." } );
 		const slugged = this.slug( title );
 		console.log( slugged )
